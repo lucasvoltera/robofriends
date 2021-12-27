4d6fc6d7d508ec3ba437e3c0b6f5f4b7a6c70ad2
Lucas Voltera <lucasvoltera@gmail.com>
2021-12-27 14:43:52 -0300

    Updates

---
+++ precache-manifest.767c39f52c0cb5fb53465011d1dea2e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d162de7def2cdd1a7e",
+    "url": "/robofriends/static/css/main.804b9e8f.chunk.css"
+  },
+  {
+    "revision": "02d162de7def2cdd1a7e",
+    "url": "/robofriends/static/js/main.02d162de.chunk.js"
+  },
+  {
+    "revision": "329df3e5463f2b354e5a",
+    "url": "/robofriends/static/js/1.329df3e5.chunk.js"
+  },
+  {
+    "revision": "109240d627e6d8c22a47",
+    "url": "/robofriends/static/css/2.c96493e9.chunk.css"
+  },
+  {
+    "revision": "109240d627e6d8c22a47",
+    "url": "/robofriends/static/js/2.109240d6.chunk.js"
+  },
+  {
+    "revision": "8252e9923ad3f927daf0",
+    "url": "/robofriends/static/js/runtime~main.8252e992.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3d6882cd225a828eea0efb4eb289292b",
+    "url": "/robofriends/index.html"
+  }
+];
